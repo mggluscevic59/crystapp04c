411472ec6ae199aae2f90659742545642e8cc344
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-02-15 09:18:59 +0100

    Intro tests completed
    3/3

--- crystapp04c/changer.py
+++ crystapp04c/changer.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 class Changer:
     def __init__(self, path) -> None:
-        self.path = Path(path)
-        self.files = []
-        if self.path.suffix == "csv":
-            self.files = [self.path]
-        elif self.path.is_dir():
-            for child in self.path.iterdir():
-                self.files.append(child)
-        else:
-            raise FileNotFoundError("File is not csv nor directory")
+        self.files = self.path_to_list(path)
+       
+    def path_to_list(self, new_path) -> list[Path]:
+        path = Path(new_path)
+        # is CSV
+        if path.suffix == ".csv":
+            return [path]
+        elif path.is_dir():
+            return [child for child in path.iterdir()]
+        raise FileNotFoundError("File is not csv nor directory")
--- tests/test_changer.py
+++ tests/test_changer.py
@@ -9,12 +9,12 @@ class changerTester(unittest.TestCase):
         return super().setUp()
 
     def test_csv_to_list(self):
-        # changer = Changer("tests/.test.csv")
-        # self._log.debug("First list: %s", changer.path)
-        # self.assertIsInstance(changer.files, list)
+        changer = Changer("tests/.test.csv")
+        self._log.debug("test_csv_to_list: %s", changer.files)
+        self.assertIsInstance(changer.files, list)
 
         changer = Changer("tests/")
         self.assertIsInstance(changer.files, list)
         self._log.debug("test_csv_to_list: %s", changer.files)
 
-        # self.assertRaises(FileNotFoundError, lambda: Changer("run_tests.py"))
+        self.assertRaises(FileNotFoundError, lambda: Changer("run_tests.py"))
