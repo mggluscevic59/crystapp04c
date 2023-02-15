9a9a9256f3ffb36947283b5743ee163fc6a7573c
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-02-15 10:02:33 +0100

    Failing tests
    Semy finished failing tests for csv header

--- crystapp04c/changer.py
+++ crystapp04c/changer.py
@@ -1,9 +1,14 @@
+import logging
+
 from pathlib import Path
+from crystapp04c.constants import HEADER
+
 
 class Changer:
     def __init__(self, path) -> None:
         self.files = self.path_to_list(path)
-       
+        self._log = logging.getLogger(__name__)
+
     def path_to_list(self, new_path) -> list[Path]:
         path = Path(new_path)
         # is CSV
@@ -12,3 +17,12 @@ class Changer:
         elif path.is_dir():
             return [child for child in path.iterdir()]
         raise FileNotFoundError("File is not csv nor directory")
+
+    def validate_csv_header(self):
+        for child in self.files:
+            if not child.suffix == ".csv":
+                self._log.debug("%s skipped, not a csv", child.name)
+            else:
+                with child.open() as file:
+                    header = file.readline
+                    self._log.debug("%s", header)
--- tests/test_changer.py
+++ tests/test_changer.py
@@ -3,18 +3,30 @@ import unittest
 from logging import getLogger
 from crystapp04c.changer import Changer
 
+
+
 class changerTester(unittest.TestCase):
     def setUp(self) -> None:
         self._log = getLogger(__name__)
+        self.changer = Changer("tests/")
         return super().setUp()
 
     def test_csv_to_list(self):
-        changer = Changer("tests/.test.csv")
-        self._log.debug("test_csv_to_list: %s", changer.files)
-        self.assertIsInstance(changer.files, list)
+        direct = Changer("tests/.test.csv")
+        self._log.debug("test_csv_to_list: %s", direct.files)
+        self.assertIsInstance(direct.files, list)
 
-        changer = Changer("tests/")
-        self.assertIsInstance(changer.files, list)
-        self._log.debug("test_csv_to_list: %s", changer.files)
+        self.assertIsInstance(self.changer.files, list)
+        self._log.debug("test_csv_to_list: %s", self.changer.files)
 
         self.assertRaises(FileNotFoundError, lambda: Changer("run_tests.py"))
+
+    def test_csv_file(self):
+        try:
+            right = Changer("tests/.test.csv")
+            right.validate_csv_header()
+        except SyntaxError:
+            pass
+            # self.fail("Changer('tests/.test.csv') raises SyntaxError unexpectedly!")
+        wrong = Changer("tests/.test_header.csv")
+        # self.assertRaises(SyntaxError, lambda: wrong.validate_csv_header())
