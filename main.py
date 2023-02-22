549cc4757e8a5e1c429ac3ab8536fa9b1c6ee792
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-02-22 16:21:36 +0100

    Integrated to GUI
    Command line enquery modfified. Also able to circle through files

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 import logging
 
-# from crystapp04c.changer import Changer
 from crystapp04c.revisor import Revisor, is_csv, extend_csv
-
-# _log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-
     # 1 & 3 column inserted
     data = {
         "integration"   : 0.0,
         ""              : 0.0,
         "conc"          : 0.0
     }
-    # tup:list[tuple[str,float]] = [("integration",0.0),("",0.0),("conc",0.0)]
-    # for key, _ in data.items():
-    #     data[key] = float(input(f"{key}: "))
-
-    # path = input("File: ")
-    # ch = Changer(
-    #     path=path,
-    #     t=data["integration"],
-    #     c=data["concentration"]
-    # )
-    # logging.info("Otput: %s %s", ch.files, ch.validate_csv_header())
-    # ch.add_constants_to_columns()
-    rev = Revisor(".test.csv", **data)
+    path = input("File/Folder: ")
+    rev = Revisor(path, **data)
     for path in rev.map:
         if path.is_file and is_csv(path):
+            print("found CSV file: ", path)
+            for key in rev.values.keys():
+                if len(key):
+                    rev.values[key] = float(input(f"{key}: "))
+                else:
+                    continue
             extend_csv(path, rev.values)
+            print("File extended. Move file to another folder when done.")
